74ea997de1c310bf5f25d0c8cda4808d8f60c5d8
nayelide <123520811+nayelide@users.noreply.github.com>
2023-02-06 23:27:21 -0800

    Update tcp_client.py

--- tcp_client.py
+++ tcp_client.py
@@ -9,10 +9,26 @@ import socket
 
 def main():
     # TODO: Create a socket and connect it to the server at the designated IP and port
+    # echo-client.py
+
+    import socket
+
+    HOST = "172.20.10.8"  # The server's hostname or IP address (Rpi_)
+
+    PORT = 8000  # The port used by the server
+
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((HOST, PORT))
+
     # TODO: Get user input and send it to the server using your TCP socket
+        s.sendall(b"Hello, world")
+        data = s.recv(1024)
+
     # TODO: Receive a response from the server and close the TCP connection
+    print(f"Received {data!r}")
+
     pass
 
 
 if __name__ == '__main__':
-    main()
+    main()()
